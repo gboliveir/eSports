27782942301db04be21639a1fec42259c37e933e
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-09-16 20:39:13 -0300

    feat(mobile): connection with api

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -3,7 +3,6 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
   ImageBackground,
-  ImageSourcePropType,
   Text
 } from 'react-native';
 
@@ -14,9 +13,11 @@ import { THEME } from '../../theme';
  
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  }
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -28,18 +29,18 @@ interface Props extends TouchableOpacityProps {
      <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
       >
         <LinearGradient
           colors={THEME.COLORS.FOOTER}
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anuncios
+            {data._count.ads} anuncios
           </Text>
         </LinearGradient>
       </ImageBackground>
