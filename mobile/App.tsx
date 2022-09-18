54abf0b59a70c1bad9044de26816459b55d84c85
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-09-17 22:44:28 -0300

    feat(mobile): add notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
 import { StatusBar } from 'react-native';
 
 import {
@@ -7,10 +9,14 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter';
+import { Subscription } from 'expo-modules-core';
 
 import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 import { Background } from './src/components/Background';
+
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +25,36 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications
+      .addNotificationReceivedListener(notification => {
+        console.log(notification);
+      })
+
+    responseNotificationListener.current = Notifications
+      .addNotificationReceivedListener(response => {
+        console.log(response);
+      })
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription( responseNotificationListener.current)
+      }
+    }
+  });
+
+
+  // ExponentPushToken[UxMa7GHXs_SRVIkxc5SgDv]
+
 
   return (
     <Background>
